4704340637269ed0a1a67147b8efe9ae8ba655fc
Zachary Littrell <mysticcabbage@gmail.com>
2012-12-05 13:38:08 -0500

    Removed some mutability from physics-test

--- test/poggio/test/jme_clj/physics_test.clj
+++ test/poggio/test/jme_clj/physics_test.clj
@@ -11,55 +11,45 @@
            [com.jme3.scene.shape Box Sphere Sphere$TextureMode]
            [com.jme3.texture Texture$WrapMode]))
 
-(def wall-mat (atom nil))
-(def stone-mat (atom nil))
-(def floor-mat (atom nil))
-
-(defn set-up-materials! [app]
+(defn materials [app]
   (let [assets (.getAssetManager app)]
-    (swap! wall-mat
-           (constantly 
-             (material :asset-manager assets
-                       :def-name "Common/MatDefs/Misc/Unshaded.j3md"
-                       :texture {"ColorMap" 
-                                 (texture 
-                                   :asset-manager assets
-                                   :texture-key 
-                                     (texture-key 
-                                      :name "Textures/Terrain/BrickWall/BrickWall.jpg"
-                                      :generate-mips true))})))
-     (swap! stone-mat
-           (constantly 
-             (material :asset-manager assets
-                       :def-name "Common/MatDefs/Misc/Unshaded.j3md"
-                       :texture {"ColorMap" 
-                                 (texture
-                                   :asset-manager assets
-                                   :texture-key
-                                     (texture-key 
-                                      :name "Textures/Terrain/Rock/Rock.PNG"
-                                      :generate-mips true))})))
-      (swap! floor-mat
-           (constantly 
-             (material :asset-manager assets
-                       :def-name "Common/MatDefs/Misc/Unshaded.j3md"
-                       :texture {"ColorMap" 
-                                 (texture
-                                  :asset-manager assets
-                                  :texture-key 
-                                   (texture-key 
-                                    :name "Textures/Terrain/Pond/Pond.jpg"
-                                    :generate-mips true)
-                                  :wrap :repeat)})))))
+    [(material :asset-manager assets
+                :def-name "Common/MatDefs/Misc/Unshaded.j3md"
+                :texture {"ColorMap" 
+                          (texture 
+                            :asset-manager assets
+                            :texture-key 
+                              (texture-key 
+                               :name "Textures/Terrain/BrickWall/BrickWall.jpg"
+                               :generate-mips true))})
+      (material :asset-manager assets
+                :def-name "Common/MatDefs/Misc/Unshaded.j3md"
+                :texture {"ColorMap" 
+                          (texture
+                            :asset-manager assets
+                            :texture-key
+                              (texture-key 
+                               :name "Textures/Terrain/Rock/Rock.PNG"
+                               :generate-mips true))})
+     (material :asset-manager assets
+                :def-name "Common/MatDefs/Misc/Unshaded.j3md"
+                :texture {"ColorMap" 
+                          (texture
+                           :asset-manager assets
+                           :texture-key 
+                            (texture-key 
+                             :name "Textures/Terrain/Pond/Pond.jpg"
+                             :generate-mips true)
+                           :wrap :repeat)})]))
             
 (def floor-shape (doto (Box. Vector3f/ZERO (float 10) (float 0.1) (float 5))
                    (.scaleTextureCoordinates (Vector2f. 3 6))))
 
-(defn set-up-floor! [app]
+(defn set-up-floor! [app floor-mat]
   (attach! app
            (geom :label "Floor"
                  :shape floor-shape
-                 :material @floor-mat
+                 :material floor-mat
                  :local-translation (Vector3f. 0 (float 0.1) 0)
                  :controls [(RigidBodyControl. (float 0))])))
 
@@ -70,15 +60,15 @@
 (def box (doto (Box. Vector3f/ZERO brick-length brick-height brick-width)
            (.scaleTextureCoordinates (Vector2f. (float 1) (float 0.5)))))
 
-(defn add-brick! [app loc]
+(defn add-brick! [app loc brick-mat]
   (attach! app
            (geom :label "Brick"
                  :shape box
-                 :material @wall-mat
+                 :material brick-mat
                  :local-translation loc
                  :controls [(RigidBodyControl. (float 2))])))
 
-(defn set-up-wall! [app]
+(defn set-up-wall! [app wall-mat]
   (loop [start (/ brick-length 4)
          height 0
          j      0]
@@ -87,7 +77,8 @@
         (add-brick! app (Vector3f. (+ (* i brick-length 2)
                                       start)
                                    (+ brick-height height)
-                                   0)))
+                                   0)
+                    wall-mat))
       (recur (- start)
              (+ height (* 2 brick-height))
              (inc j)))))
@@ -117,12 +108,12 @@
 (def sphere (doto (Sphere. 32 32 (float 0.4) true false)
               (.setTextureMode Sphere$TextureMode/Projected)))
 
-(defn make-cannon-ball! [app]
+(defn make-cannon-ball! [app ball-mat]
   (let [ball-phys (RigidBodyControl. (float 1))]
     (attach! app
              (geom :label "CannonBall"
                    :shape sphere
-                   :material @stone-mat
+                   :material ball-mat
                    :local-translation (.getLocation (.getCamera app))
                    :controls [ball-phys]))
     (.setLinearVelocity ball-phys (.mult (.getDirection (.getCamera app))
@@ -132,21 +123,21 @@
 (defn make-app []
   (proxy [SimpleApplication] []
     (simpleInitApp []
-      (doto (.getStateManager this)
-        (.attach (BulletAppState.)))
-      (doto (.getCamera this)
-        (.setLocation (Vector3f. 0 (float 4) (float 6)))
-        (.lookAt (Vector3f. 2 2 0) Vector3f/UNIT_Y))
-      (doto (.getInputManager this)
-        (on-mouse-button!* :action MouseInput/BUTTON_LEFT
-          [_ name is-pressed? tpf]
-            (when-not is-pressed?
-              (make-cannon-ball! this))))
-      (doto this
-        (set-up-materials!)
-        (set-up-wall!)
-        (set-up-floor!)
-        (set-up-cross-hairs!)))))
+      (let [[brick-mat ball-mat floor-mat] (materials this)]
+        (doto (.getStateManager this)
+          (.attach (BulletAppState.)))
+        (doto (.getCamera this)
+          (.setLocation (Vector3f. 0 (float 4) (float 6)))
+          (.lookAt (Vector3f. 2 2 0) Vector3f/UNIT_Y))
+        (doto (.getInputManager this)
+          (on-mouse-button!* :action MouseInput/BUTTON_LEFT
+            [_ name is-pressed? tpf]
+              (when-not is-pressed?
+                (make-cannon-ball! this ball-mat))))
+        (doto this
+          (set-up-wall! brick-mat)
+          (set-up-floor! floor-mat)
+          (set-up-cross-hairs!))))))
 
 (defn -main [& args]
   (doto (make-app)

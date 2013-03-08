462fd417f47342cf68ecb95ce0a72965f51e6192
Zachary Littrell <mysticcabbage@gmail.com>
2013-03-07 22:12:40 -0500

    Added >|_| and |_|< macros for combining juxt with -> and ->>.

--- src/data/coll.clj
+++ src/data/coll.clj
@@ -4,3 +4,20 @@
   "Returns coll with only distinct elements, where two elements are 
    equal if they have the same output from f."
   (map first (vals (group-by f coll))))
+
+(defmacro >|_| [obj & forms]
+  "Returns a vector starting with obj, followed by the result of
+   computing each form with obj inserted after the first item."
+  (let [obj* (gensym "obj")
+        forms* (for [[head & form*] forms] (list* head obj* form*))]
+  `(let [~obj* ~obj]
+     [~obj* ~@forms*])))
+
+(defmacro |_|< [obj & forms]
+  "Returns a vector starting with obj, followed by the result of
+   computing each form with obj appended."
+  (let [obj* (gensym "obj")
+        forms* (for [form forms] (concat form [obj*]))]
+    `(let [~obj* ~obj]
+       [~obj* ~@forms*])))
+

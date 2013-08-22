42ffa04031e82d7b1e47affd11dbf0e22b5133c4
Zachary Littrell <mysticcabbage@gmail.com>
2013-08-22 01:47:10 -0400

    Modified a few tests

--- test/poggio/test/nifty_clj/popup_test.clj
+++ test/poggio/test/nifty_clj/popup_test.clj
@@ -5,10 +5,23 @@
 
 (defn my-screen [nifty]
   (build-screen nifty
-         (screen-around
+         (laid-out-screen-around
+           :horizontal
            (button :label "Click me!"
-                   :id "btn"))
-         :btn {:on-left-click #(alert! nifty "HOWDY!\nThis is an alert.")}))
+                   :id "btn")
+           (button label "Or me!"
+                   :id "better-btn")
+           (button label "I have more to say!"
+                   :id "scroll-btn"))
+         :btn {:on-left-click #(println (alert! nifty "HOWDY!\nThis is an alert."))}
+         :scroll-btn {:on-left-click
+                      #(alert! nifty "HOWDY\nTHIS IS MY MESSAGE FROM ME TO\n\n\n\n\n\n\n\n\n\n\n\n\nYou!" :scroll? true)}
+         :better-btn {:on-left-click
+                      #(popup! nifty (.getCurrentScreen nifty)
+                              (panel :child-layout :center
+                                     :text (text :text "HI!"))
+                              :continuation (fn [_ val]
+                                              (println val)))}))
 
 (defn make-app []
   (proxy [SimpleApplication] []
@@ -17,7 +30,7 @@
         (.detachAllChildren))
       (doto (.getFlyByCamera this)
         (.setDragToRotate true))
-      (let [nifty (nifty this true)]
+      (let [[_ nifty] (nifty this true)]
         (.addScreen nifty "my-screen" (my-screen nifty))
         (.gotoScreen nifty "my-screen")))))
 
--- test/poggio/test/poggio/functions/functions_test.clj
+++ test/poggio/test/poggio/functions/functions_test.clj
@@ -1,22 +1,65 @@
 (ns poggio.test.poggio.functions.functions-test
   (:use [clojure.test]
-        [poggio.functions core value list]))
+        [poggio.functions core scenegraph number value list]))
 
-(deftest seq->pog-fn-test2
-  (let [triple* (seq->pog-fn "triple" ["a" "b" "c"] (list cons* "a"
-                                                 (list cons* "b"
-                                                       (list cons* "c"
-                                                             empty-list*))))
-        inc* (fn->pog-fn inc' "inc" ["n"])
-        xs (invoke triple* [1 2 3])]
-    (is (= (invoke map* [inc* (constantly* xs)])
-           (list 2 3 4)))))
+;;(deftest var-test
+;;  (is (= (value (var* "foo") {"foo" (constantly* 3)})
+;;         3)))
+;;
+;;(deftest let-test
+;;  (is (value (invoke* let* {} [(var* "foo") (constantly* 3) (var* "foo")]) {})
+;;      3))
+;;
+;;(deftest cons-test
+;;  (is (= (invoke* cons* {} [(constantly* 3) (constantly* (list))])
+;;         `(3))))
+(deftest seq-test
+    (is (= (invoke* inc* {} [(constantly* 3)])
+           4)))
 
-(deftest seq->pog-fn-test
-  (let [triple* (seq->pog-fn "triple" ["a" "b" "c"]
-                  (list cons* "a"
-                        (list cons* "b"
-                              (list cons* "c" empty-list*))))]
-    (is (= (invoke triple* [1 2 3])
-           (list 1 2 3)))))
+(deftest repeat-test
+  (is (= (take 3 (invoke* repeat* {} [(constantly* 3)]))
+         '(3 3 3))))
 
+(deftest map-test
+  (is (= (invoke* map* {} [inc* (constantly* (list 1 2 3))])
+         '(2 3 4))))
+
+(deftest partial-test
+  (is (= (invoke* (partial* inc* {"n" (constantly* 3)}) []) 4))
+  (is (= (invoke* (partial* inc* {}) [(constantly* 3)]) 4)))
+
+(deftest pog-fn-node-test
+  (let [pfn (pog-fn-node :pog-fn inc*)
+        pfn2 (pog-fn-node :pog-fn add*)]
+    (is (= (invoke* pfn [(constantly* 3)])
+           4))
+    (is 
+        (= (invoke* (partial* pfn {}) [(constantly* 3)])
+           4))
+    (is 
+        (= (invoke* (partial* pfn {"n" (constantly* 3)}) [])
+           4))
+    (is (= (invoke* pfn2 [(constantly* 1) (constantly* 2)])
+           3))
+    (is (= (invoke* (partial* pfn2 {"n1" (constantly* 1)})
+                    [(constantly* 2)])
+           3))
+    (is (= (invoke* (partial* (partial* pfn2 {"n1" (constantly* 1)})
+                              {"n2" (constantly* 2)})
+                    [])
+           3))
+    (is (= (first (invoke* repeat* [(invoke* triple* [1 2 3])]))
+           '(1 2 3)))
+    (is (= (invoke* flatten* ['((1) (2) (3))])
+           '(1 2 3)))
+    (is (= (take 3 (invoke* flatten* [(invoke* repeat* ['(3)])]))
+           '(3 3 3)))
+
+    (is (= (second (invoke* map* [inc* (repeat 3)]))
+           4))
+
+    (is (= (first  (invoke* concat* [(repeat 3) (repeat 3)]))
+           3))
+    
+    ))

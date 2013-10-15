e0937c1143cb2f95387c571a1dc79f6857ff166a
Zachary Littrell <mysticcabbage@gmail.com>
2013-10-15 15:34:58 -0400

    Added a few more quotes.

--- src/poggio/quotes.clj
+++ src/poggio/quotes.clj
@@ -15,6 +15,8 @@
  {:author "1 Thessalonians 5:21"
   :quote "Test everything; retain what is good."}
  {:author "Edsger Dijkstra"
+  :quote "The tools we use have a profound (and devious!) influence on our thinking habits, and, therefore, on our thinking abilities."}
+ {:author "Edsger Dijkstra"
   :quote "The lurking suspicion that something could be simplied is the world's richest source of rewarding challenges."}
  {:author "Alan Kay"
   :quote "A change in perspective is worth 80 IQ points."}
@@ -28,4 +30,12 @@
   :quote "It is not enough to be in the right place at the right time.\nYou should also have an open mind at the right time."}
  {:author "Alan Perlis"
   :quote "I hope the field of computer science never loses its sense of fun."}
+ {:author "Alan Kay"
+  :quote "A change in perspective is worth 80 IQ points."}
+ {:author "Galileo"
+  :quote "All truths are easy to understand once they are discovered; the point is to discover them."}
+ {:author "Samuel Smiles"
+  :quote "We often discover what will do, by finding out what will not do; and probably he who never made a mistake never made a discovery."}
+ {:author "Euclid"
+  :quote "The whole is greater than the part."}
  ])
